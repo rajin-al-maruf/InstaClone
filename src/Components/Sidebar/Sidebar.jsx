2061e03e8184f780ff37b56892af48c74a7ffeaa
Rajin Al Maruf <rajinalmarufananda@gmail.com>
2025-02-02 00:48:55 +0600

    sidebar update

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,56 +1,27 @@
-import {Box, Flex, Link, Avatar, Tooltip, Button} from '@chakra-ui/react'
-import { Link as RouterLink } from 'react-router-dom'
-import { CreatePostLogo, 
-  InstagramLogo, 
-  InstagramMobileLogo, 
-  NotificationsLogo, 
-  SearchLogo } from '../../assets/contants'
-import { AiFillHome } from "react-icons/ai";
-import { BiLogOut } from 'react-icons/bi'
+import {Box, Flex, Link, Tooltip, Button} from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom';
+import { InstagramLogo, InstagramMobileLogo, } from '../../assets/contants';
+import { BiLogOut } from 'react-icons/bi';
 import useLogout from '../../hooks/useLogout';
+import SidebarItems from './SidebarItems';
 
 
 
 const Sidebar = () => {
 
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25}/>,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo />,
-      text: "Search",
-    },
-    {
-      icon: <NotificationsLogo />,
-      text: "Notifications",
-    },
-    {
-      icon: <CreatePostLogo />,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size="sm" name="Burak Orkmez" src="/profilepic.png"/>,
-      text: "Profile",
-      link: "/asaprogrammer",
-    },
-  
-  ]
   
   const { handleLogout, isLoggingOut} = useLogout()
 
   return (
         <Box 
-        h={"100vh"} 
-        borderRight={"1px solid"} 
-        borderColor={"whiteAlpha.300"}  
-        px={{base: 2, md: 4}} 
-        py={8} 
-        position={"sticky"} 
-        top={0} 
-        left={0}
+          h={"100vh"} 
+          borderRight={"1px solid"} 
+          borderColor={"whiteAlpha.300"}  
+          px={{base: 2, md: 4}} 
+          py={8} 
+          position={"sticky"} 
+          top={0} 
+          left={0}
         > 
 
           <Flex direction={"column"} gap={10} h={"full"} w={"full"} alignItems={{base: "center", md: "flex-start"}}>
@@ -64,36 +35,7 @@ const Sidebar = () => {
             </Link>
 {/* Home, search, noti, post, profile */}
             <Flex direction={"column"} gap={5} cursor={"pointer"}>
-              {sidebarItems.map((item, index) => (
-                <Tooltip
-                  key={index}
-                  label= {item.text}
-                  hasArrow
-                  placement="right"
-                  positioning={{placement: "right"}}
-                  openDelay={500}
-                  display={{base: "block", md: "none"}}
-                  ml={1}
-                >
-                  <Link
-                    key={index}
-                    display={"flex"}
-                    variant={"unstyled"}
-                    to={item.link || null}
-                    as={RouterLink}
-                    alignItems={"center"}
-                    gap={4}
-                    p={2}
-                    borderRadius={6}
-                    _hover={{bg: "whiteAlpha.100"}}
-                    w={{base: 10, md: 200}}
-                    justifyContent={{base: "center", md: "flex-start"}}
-                  >
-                    {item.icon}
-                    <Box display={{base: "none", md: "block"}}>{item.text}</Box>
-                  </Link>
-                </Tooltip>
-              ))}
+              <SidebarItems />
             </Flex>
 {/* Logout */}
                 <Tooltip
